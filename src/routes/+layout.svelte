04eb35e068822a7476a5bb69ada92730ffd48303
Keko <hmepo.dev@gmail.com>
2026-06-29 07:14:14 -0300

    Add: favicon and minimal metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import './layout.css';
+	import favicon from '$lib/assets/favicon.ico';
 
 	let { children } = $props();
 </script>
 
-<svelte:head></svelte:head>
+<svelte:head>
+	<link rel="icon" type="image/png" href="{favicon}" />
+	<meta name="title" content="Revista Digital IPS" />
+	<meta name="description" content="Accede a todos los números previos, lee los resúmenes informativos o descarga las ediciones completas en formato PDF." />
+</svelte:head>
 {@render children()}
